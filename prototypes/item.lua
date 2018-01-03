dc9f1dedbbe5ba22fe83e8142a4e12668474aedc
Axel Andrén <axelandren@gmail.com>
2018-01-03 21:41:09 +0100

    0.16 update

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -4,6 +4,7 @@ data:extend({
     type = "item",
     name = "aquifer-drill",
     icon = "__AquiferDrill__/graphics/icons/aquifer-drill.png",
+    icon_size = 32,
     flags = { "goes-to-quickbar" },
     subgroup = "extraction-machine",
     place_result = "aquifer-drill",
